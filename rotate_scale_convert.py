5ca06b28d67c5218bb6b7dc7766f224b70f844f3
H. P. Gross <hpgross@gmail.com>
2021-05-10 08:52:50 -0400

    Set up the basic script structure\
    Imported my dependencies, set up a framework for the io
     and set out sections where each image operation will go

--- rotate_scale_convert.py
+++ rotate_scale_convert.py
@@ -1,8 +1,23 @@
 #/usr/bin/python3
 
-import PIL
 import os
+from PIL import Image
 
 # This file will iterate over a series of images
 # scaling them down, rotating them by 90 degrees
 # and then converting them to a different format
+
+src_dir = "" #The file path for the source directory
+prt_dir = "" #The file path for the output directory
+
+for filename in src_dir:
+    path = src_dir + "/" + filename
+    im = image.open(path)
+
+    #scale
+
+    #rotate
+
+    #convert
+
+    #output
