a2d892e143d2bbbb08ddb6205e2979ffd1f3164d
hpgross <hpgross@gmail.com>
2021-05-11 12:43:07 -0400

    Set up the main structure of the converter
    Added source and destination directories, added the main functionality
    
    Fixed a broken #! at the start

--- rotate_scale_convert.py
+++ rotate_scale_convert.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 
 import os
 from PIL import Image
@@ -7,17 +7,15 @@ from PIL import Image
 # scaling them down, rotating them by 90 degrees
 # and then converting them to a different format
 
-src_dir = "" #The file path for the source directory
-prt_dir = "" #The file path for the output directory
+src_dir = "/home/student-00-de2bf90de6eb/images" #The file path for the source directory
+prt_dir = "/home/student-00-de2bf90de6eb/opt/icons" #The file path for the output directory
 
-for filename in src_dir:
-    path = src_dir + "/" + filename
-    im = image.open(path)
-
-    #scale
-
-    #rotate
-
-    #convert
-
-    #output
+for filename in os.listdir(src_dir):
+    src_path = src_dir + "/" + filename
+    if src_path[-4:] == "48dp":
+        prt_path = prt_dir + "/" + filename
+        im = Image.open(src_path)
+        im = im.resize((128,128))
+        im = im.rotate(270)
+        im = im.convert("RGB")
+        im.save(prt_path,"JPEG")
